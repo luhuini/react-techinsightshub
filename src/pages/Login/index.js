34294b47430e72c3ba47d5d0a2e1857b773cfa4e
Lu <stupidlifewww@foxmail.com>
2024-09-18 14:27:06 +0100

    login page initial

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,5 +1,27 @@
+import { Button, Card, Form, Input } from "antd";
+import "./index.scss";
+import evil from "@/assets/evil.jpg";
 const Login = () => {
-  return <h1>i am login</h1>;
+  return (
+    <div>
+      <Card>
+        <img src={evil} alt="logo" />
+        <Form>
+          <Form.Item>
+            <Input size="large" placeholder="plase enter phone number"></Input>
+          </Form.Item>
+          <Form.Item>
+            <Input size="large" placeholder="plase enter auth code"></Input>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" size="large" block>
+              login
+            </Button>
+          </Form.Item>
+        </Form>
+      </Card>
+    </div>
+  );
 };
 
 export default Login;
